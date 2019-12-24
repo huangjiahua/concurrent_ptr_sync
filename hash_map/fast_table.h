5083ebcb15cf0b77e527f4f15bbd0a3bef67bdde
lwhay <lwh@whu.edu.cn>
2019-12-24 23:38:02 +0800

    improve fast_table

--- hash_map/fast_table.h
+++ hash_map/fast_table.h
@@ -47,11 +47,43 @@ public:
 template<typename KeyType, typename ValueType>
 class FastTable {
 private:
+    constexpr static uintptr_t kValidPtrField = 0x0000ffffffffffffull;
     template<typename T> using Atom = std::atomic<T>;
     using Node = FastTableNode<KeyType, ValueType>;
     using Slot = FastTableSlot<Node>;
     size_t size_;
     Slot *table_;
+
+    static Node *FilterValidPtr(Node *ptr) {
+        return (Node *)(((uintptr_t)ptr) & kValidPtrField);
+    }
+
+    static size_t GetSecondLevelHash(size_t h) {
+        return (h >> 16ull) & 0xffffull;
+    }
+
+    static size_t GetSecondLevelHashFromPtr(Node *ptr) {
+        return (((size_t)ptr) >> 48ull);
+    }
+
+    static Node *MarkSecondLevelHash(Node *ptr, size_t hash) {
+        size_t ptr_int = (size_t)ptr;
+        size_t sec_hash = GetSecondLevelHash(hash);
+        return (Node *)((sec_hash << 48ull) | ptr_int);
+    }
+
+    class CompareSecondLevelHash {
+    private: 
+        size_t second_level_hash_;
+    public:
+        CompareSecondLevelHash(size_t hash): 
+            second_level_hash_(GetSecondLevelHash(hash)) {}
+        
+        bool operator()(Node *ptr) {
+            size_t hash = GetSecondLevelHashFromPtr(ptr);
+            return (hash == second_level_hash_);
+        }
+    };
 public:
     // size must be power of 2
     explicit FastTable(size_t size) : size_(size) {
@@ -71,19 +103,28 @@ public:
     void Insert(size_t hash, const KeyType &key, const ValueType &value) {
         Node *node = new Node(hash, key, value);
         size_t idx = GetIdx(hash);
-        Node *old = table_[idx].atom_ptr_.exchange(node);
+        Node *old = table_[idx].atom_ptr_.exchange(MarkSecondLevelHash(node, hash));
         if (old) HazPtrRetire(old);
     }
 
     bool CheckedInsert(size_t hash, const KeyType &key, const ValueType &value) {
         size_t idx = GetIdx(hash);
         HazPtrHolder holder;
-        Node *old_node = holder.Repin(table_[idx].atom_ptr_);
-        if (old_node && (old_node->Hash() != hash || old_node->Key() != key)) {
+
+        Node *old_node = holder.Repin(table_[idx].atom_ptr_, CompareSecondLevelHash(hash), 
+            [](Node *p){return p;});
+        if (GetSecondLevelHash(hash) != GetSecondLevelHashFromPtr(old_node)) {
+            return false;
+        }
+
+        Node *node_ptr = FilterValidPtr(old_node);
+
+        if (node_ptr && (node_ptr->Hash() != hash || node_ptr->Key() != key)) {
             return false;
         }
         Node *node = new Node(hash, key, value);
-        bool res = table_[idx].atom_ptr_.compare_exchange_strong(old_node, node, std::memory_order_acq_rel);
+        bool res = table_[idx].atom_ptr_.compare_exchange_strong(old_node, 
+            MarkSecondLevelHash(node, hash), std::memory_order_acq_rel);
         if (!res) {
             delete node;
         } else {
@@ -95,10 +136,18 @@ public:
     bool TryUpdate(size_t hash, const KeyType &key, const ValueType &value) {
         size_t idx = GetIdx(hash);
         HazPtrHolder holder;
-        Node *old_node = holder.Repin(table_[idx].atom_ptr_);
-        if (old_node && old_node->Hash() == hash && old_node->Key() == key) {
+        Node *old_node = holder.Repin(table_[idx].atom_ptr_, CompareSecondLevelHash(hash), 
+            [](Node *p){return p;});
+        if (GetSecondLevelHash(hash) != GetSecondLevelHashFromPtr(old_node)) {
+            return false;
+        }
+
+        Node *node_ptr = FilterValidPtr(old_node);
+
+        if (node_ptr && node_ptr->Hash() == hash && node_ptr->Key() == key) {
             Node *node = new Node(hash, key, value);
-            bool res = table_[idx].atom_ptr_.compare_exchange_strong(old_node, node, std::memory_order_acq_rel);
+            bool res = table_[idx].atom_ptr_.compare_exchange_strong(old_node, 
+                MarkSecondLevelHash(node, hash), std::memory_order_acq_rel);
             if (!res) {
                 delete node;
             } else {
@@ -112,7 +161,12 @@ public:
     Node *PinnedFind(size_t hash, const KeyType &key) {
         HazPtrHolder holder;
         size_t idx = GetIdx(hash);
-        Node *node = holder.Repin(table_[idx].atom_ptr_);
+        Node *node = holder.Repin(table_[idx].atom_ptr_, CompareSecondLevelHash(hash),
+            [](Node *p){return p;});
+        if (GetSecondLevelHash(hash) != GetSecondLevelHashFromPtr(node)) {
+            return nullptr;
+        }
+        node = FilterValidPtr(node);
         if (!node || node->Key() != key) {
             return nullptr;
         }
